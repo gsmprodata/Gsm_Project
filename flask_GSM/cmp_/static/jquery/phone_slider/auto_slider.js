b5561958553e929874c5fa4e40793381ec48a5ec
230195 <33318181+230195@users.noreply.github.com>
2020-01-19 02:42:57 +0530

    auto sliding functionality with mouse enter and leave checks

--- flask_GSM/cmp_/static/jquery/phone_slider/auto_slider.js
+++ flask_GSM/cmp_/static/jquery/phone_slider/auto_slider.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
     var animateDelay = 300;
+    var autoScrollTimer = 1500;
+    var isSliderPaused = false;
 
     function slide_left() {
-
-    }
-
-    function slide_right() {
-
-    }
-
-    $('.corousel-container .slider-arrow .previous').click(function(e) {
-        let $parent = $(e.currentTarget).parent();
+        let $parent = $('.corousel-container .slider-arrow .previous').parent()
         let $row;
+        let auto_scroll_left;
         let sibling_width;
         let current_scroll;
         if ($parent.length > 0) {
@@ -23,15 +18,19 @@ $(document).ready(function() {
         current_scroll = $row.scrollLeft();
         if (current_scroll - sibling_width <= 0) {
             $row.animate({ scrollLeft: 0 }, animateDelay, 'swing');
+            auto_scroll_left = false;
         } else {
             $row.animate({ scrollLeft: current_scroll - sibling_width }, animateDelay, 'swing');
+            auto_scroll_left = true;
         }
-    });
+        return auto_scroll_left;
+    }
 
-    $('.corousel-container .slider-arrow .next').click(function(e) {
-        let $parent = $(e.currentTarget).parent();
+    function slide_right() {
+        let $parent = $('.corousel-container .slider-arrow .next').parent()
         let $row;
         let sibling_width;
+        let auto_scroll_right;
         let current_scroll;
         let scroll_width;
         if ($parent.length > 0) {
@@ -44,8 +43,68 @@ $(document).ready(function() {
         scroll_width = $row.length > 0 ? $row[0].scrollWidth - $row[0].clientWidth : 0;
         if (current_scroll + sibling_width <= scroll_width) {
             $row.animate({ scrollLeft: current_scroll + sibling_width }, animateDelay, 'swing');
+            auto_scroll_right = true;
         } else {
             $row.animate({ scrollLeft: scroll_width }, animateDelay, 'swing');
+            auto_scroll_right = false;
         }
+        return auto_scroll_right;
+    }
+
+    function autoScroll(auto_scroll_left, auto_scroll_right) {
+        let scroll_right;
+        let scroll_left;
+        if (auto_scroll_right == true) {
+            if (isSliderPaused) {
+                scroll_right = true;
+            } else {
+                scroll_right = slide_right();
+            }
+            setTimeout(function() {
+                if (scroll_right) {
+                    autoScroll(false, scroll_right)
+                } else {
+                    autoScroll(true, scroll_right)
+                }
+
+            }, autoScrollTimer);
+            return true;
+        }
+
+        if (auto_scroll_left) {
+            if (isSliderPaused) {
+                scroll_left = true;
+            } else {
+                scroll_left = slide_left();
+            }
+            setTimeout(function() {
+                if (scroll_left) {
+                    autoScroll(scroll_left, false)
+                } else {
+                    autoScroll(scroll_left, true)
+                }
+            }, autoScrollTimer);
+            return true;
+        }
+
+    }
+
+    setTimeout(function() {
+        autoScroll(false, true);
+    }, autoScrollTimer)
+
+    $('.corousel-container .slider-arrow .previous').click(function(e) {
+        slide_left();
     });
+
+    $('.corousel-container .slider-arrow .next').click(function(e) {
+        slide_right();
+    });
+
+    $('.corousel-container .block-content').on("mouseenter", function() {
+        isSliderPaused = true;
+    })
+    $('.corousel-container .block-content').on("mouseleave", function() {
+        isSliderPaused = false;
+    })
 });
