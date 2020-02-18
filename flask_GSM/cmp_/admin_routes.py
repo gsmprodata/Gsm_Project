aed88f7f8b862291dfdcaf55750c2b793af0ccef
anuj kanojia <anujkanojia13@gmail.com>
2020-02-19 00:16:08 +0530

    admin

--- flask_GSM/cmp_/admin_routes.py
+++ flask_GSM/cmp_/admin_routes.py
@@ -7,6 +7,15 @@ from cmp_.forms import login_form
 from cmp_ import app
 from cmp_.forms import login_form
 from cmp_.routes import get_top_phones
+import json,datetime
+
+def get_slider_device():
+    phone_list = db.session.query(top_phones).filter(top_phones.is_active == True).all()
+    list = []
+    for phone in phone_list:
+        phone_information = {"name" : phone.home_allpro.name, "phone_id":       phone.home_allpro.id, "img_name": phone.home_allpro.img_name, "slug":phone.home_allpro.slug,"brand":phone.home_allpro.brand}
+        list.append(phone_information)
+    return list
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -15,5 +24,21 @@ def login():
 
 @app.route('/dashboard',methods=['GET','POST'])
 def dashboard():
-    phone_list = get_top_phones()
+    phone_list = get_slider_device()
     return render_template('admin/slider.html',list=phone_list)
+
+@app.route('/addDevicesToslider',methods=['GET'])
+def addDevicesToslider():
+  device = request.args.get('key')
+  list_of_device = json.loads(device)
+  flag='False'
+  try:
+    for i in list_of_device:
+      db.session.add(top_phones(phone_id=int(i),is_active=True,created_at=datetime.datetime.now()))
+      db.session.commit()
+      flag = 'True'
+
+    return flag
+  except:
+    return flag
+
--- flask_GSM/cmp_/static/admin/js/adminJquery.js
+++ flask_GSM/cmp_/static/admin/js/adminJquery.js
@@ -2,12 +2,11 @@
 $(document).ready(function(){
   $('#add_device').keyup(function() {
     search_query = $('#add_device').val();
-    ul = $('#device_search_option');
+    ul = $("#device_search_option");
     ul.empty();
 
     if(search_query.length>0)
     {
-        $("#device_search_option").html("");
 
         $.ajax({
             type:"GET",
@@ -23,15 +22,40 @@ $(document).ready(function(){
                 $('.suggestedDevice').click(function(){
                     id = $(this).attr('data-click');
                     name = $(this).html();
-                    $('#selected_device').append('<div id="'+id+'" class="alert alert-success"   role="alert">'+name+' </div>')
+                    $('#selected_device').append('<div id="'+id+'" class="alert alert-success selected_device"   role="alert">'+name+' <span class="selection_close" style="float:right" aria-hidden="true">&times;</span> </div>')
+                });
+                $('#add_device_slider').click(function(){
+
+                    var selected_device_id = [];
+                    $('#selected_device').find('div').each(function(){selected_device_id.push(this.id)})
+                    $.ajax({
+                        type:'GET',
+                        url:'/addDevicesToslider',
+                        data:{'key':JSON.stringify(selected_device_id)},
+                        success:function(data)
+                        {
+                            if(data=='True')
+                            {
+                                location.reload();
+
+                            }
+                        },
+                        error:function(data)
+                        {
+                            alert(data)
+                        }
+                    });
+
+
                 });
             }
         });
     }
   });
 
-  $('.closeButton').click(function(){
-      alert('asdasd');
-  });
+//   $('.closeButton').click(function(){
+//     $('#add_new_model_content').empty();
+//     $('#add_device').val('');
+//   });
     
 });
